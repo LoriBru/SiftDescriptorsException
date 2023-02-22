de3578a23e0a297494917d26166d742525082001
LoriBru <loribru@outlook.com>
2023-02-22 09:34:30 +0100

    Add files via upload

---
+++ SiftDescriptorsException/Program.cs
@@ -0,0 +1,16 @@
+﻿using Emgu.CV.Structure;
+using Emgu.CV;
+using System;
+using SiftDescriptorsException;
+
+var i = 0;
+while (i < 1000)
+{
+    var image = new Image<Gray, byte>("CvFov.tiff");
+
+    // Compute Sift descriptors
+    (var kp, var des) = SiftComputer.ComputeSiftDescriptors(image);
+    Console.WriteLine($"i: {i}, kp: {kp.Length}, des: {des.Total}");
+    i++;
+}
+
---
+++ SiftDescriptorsException/SiftComputer.cs
@@ -0,0 +1,25 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiftDescriptorsException
+{
+    internal class SiftComputer
+    {
+        internal static (VectorOfKeyPoint kp, Mat des) ComputeSiftDescriptors(Image<Gray, byte> imageCV)
+        {
+            var featureDetector = new Emgu.CV.Features2D.SIFT();
+
+            // Extract features from the reference image
+            var kp = new VectorOfKeyPoint();
+            var des = new Mat();
+            featureDetector.DetectAndCompute(imageCV, null, kp, des, false);
+            return (kp, des);
+        }
+    }
+}
